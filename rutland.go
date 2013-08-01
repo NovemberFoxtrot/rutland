e174e0e718cb0e1f3d07e532ff7b8df3a75fb46a
NovemberFoxtrot <november@fox.trot>
2013-08-01 02:51:06 -0700

    pick a color

--- rutland.go
+++ rutland.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "flag"
 	"image"
 	"image/color"
 	_ "image/gif"
@@ -16,9 +17,28 @@ func checkerror(err error) {
 	}
 }
 
+func invertcolor(somecolor uint32, somealpha uint32) uint32 {
+	return somealpha - somecolor
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatalln("Usage: <input> <output>")
+	}
+
+	var red bool
+	var green bool
+	var blue bool
+
+	for _, value := range os.Args {
+		switch value {
+		case "red":
+			red = true
+		case "green":
+			green = true
+		case "blue":
+			blue = true
+		}
 	}
 
 	file, err := os.Open(os.Args[1])
@@ -38,9 +58,21 @@ func main() {
 
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, _, _, a := m.At(x, y).RGBA()
+			r, g, b, a := m.At(x, y).RGBA()
 
-			rm.Set(x, y, color.RGBA{uint8(a - r), 0, 0, uint8(a)})
+			if red {
+				r = invertcolor(r, a)
+			}
+
+			if green {
+				g = invertcolor(g, a)
+			}
+
+			if blue {
+				b = invertcolor(b, a)
+			}
+
+			rm.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
 		}
 	}
 
