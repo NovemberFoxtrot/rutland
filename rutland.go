040343ed95c895069865cc4028ecc5ffcc08b5f5
NovemberFoxtrot <november@fox.trot>
2013-08-02 01:38:37 -0700

    mucking around

--- rutland.go
+++ rutland.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "flag"
+	"flag"
 	"image"
 	"image/color"
 	_ "image/gif"
@@ -21,30 +21,21 @@ func invertcolor(somecolor uint32, somealpha uint32) uint32 {
 	return somealpha - somecolor
 }
 
+var (
+	red        = flag.Int("red", 0, "red percentage")
+	green      = flag.Int("green", 0, "green percentage")
+	blue       = flag.Int("blue", 0, "blue percentage")
+	inputfile  = flag.String("input", "", "blue percentage")
+	outputfile = flag.String("output", "", "blue percentage")
+)
+
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalln("Usage: <input> <output>")
-	}
+	flag.Parse()
 
-	var red bool
-	var green bool
-	var blue bool
-
-	for _, value := range os.Args {
-		switch value {
-		case "red":
-			red = true
-		case "green":
-			green = true
-		case "blue":
-			blue = true
-		}
-	}
-
-	file, err := os.Open(os.Args[1])
+	file, err := os.Open(*inputfile)
 	checkerror(err)
 
-	tofile, err := os.Create(os.Args[2])
+	tofile, err := os.Create(*outputfile)
 	checkerror(err)
 
 	defer file.Close()
@@ -60,17 +51,21 @@ func main() {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			r, g, b, a := m.At(x, y).RGBA()
 
-			if red {
+			if *red < 0 {
 				r = invertcolor(r, a)
 			}
 
-			if green {
+			if *green < 0 {
 				g = invertcolor(g, a)
 			}
 
-			if blue {
+			if *blue < 0 {
 				b = invertcolor(b, a)
 			}
+
+			r = r * uint32(*red)
+			g = g * uint32(*green)
+			b = b * uint32(*blue)
 
 			rm.Set(x, y, color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
 		}
